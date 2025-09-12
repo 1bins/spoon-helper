c09d53065724d6386de8af122ef9c4e438117da9
서원빈 <1bins@kakao.com>
2025-09-12 16:19:24 +0900

    feat: CustomDatePicker 컴포넌트 생성

---
+++ src/components/ui/CustomDatePicker/index.tsx
@@ -0,0 +1,40 @@
+import "react-datepicker/dist/react-datepicker.css";
+import styles from './customDatePicker.module.scss';
+import classnames from 'classnames/bind';
+import DatePicker, {registerLocale} from 'react-datepicker';
+import { ko } from 'date-fns/locale';
+import { FcCalendar } from "react-icons/fc";
+
+const cx =  classnames.bind(styles);
+registerLocale('ko', ko);
+
+interface CustomDatePickerProps {
+  placeholder?: string;
+  value?: null | Date;
+  onChange?: (value: null | Date) => void;
+}
+
+const CustomDatePicker = ({
+  placeholder,
+  value,
+  onChange,
+}: CustomDatePickerProps) => {
+
+  return(
+    <div className={cx('container')}>
+      <DatePicker
+        selected={value}
+        onChange={date => onChange!(date)}
+        dateFormat={"yyyy-MM-dd"}
+        className={cx('form-date')}
+        placeholderText={placeholder}
+        locale={"ko"}
+        showIcon
+        toggleCalendarOnIconClick
+        icon={<FcCalendar size={16} />}
+      />
+    </div>
+  )
+}
+
+export default CustomDatePicker;
